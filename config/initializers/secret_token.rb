187a40a95ab73ce5487f6651b0bb48238eecc732
Alexei Yuzhakov <sibprogrammer@gmail.com>
2012-01-14 11:15:35 +0600

    Initial import of empty Rails project.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Specscan::Application.config.secret_token = 'e5bd2c504cdd9c3b0290de7dd6a96cf0112a04020c38ba58b65e60c4330a7762d3e0d825bce76ac33d955e5ac4af0ddc8d39b1ec06461fd031d35ce940f86ab3'
