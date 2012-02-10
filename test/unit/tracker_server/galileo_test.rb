440a13b0bac0a8a30a8ade8c03b578de00575772
Alexei Yuzhakov <sibprogrammer@gmail.com>
2012-02-11 00:07:52 +0700

    Replace similar checks with loops.

--- test/unit/tracker_server/galileo_test.rb
+++ test/unit/tracker_server/galileo_test.rb
@@ -8,7 +8,8 @@ module TrackerServer
     end
 
     def logger
-      # do nothing
+      return @logger if @logger
+      @logger = TrackerServer::Logger.new(STDERR)
     end
   end
 end
@@ -17,6 +18,13 @@ class TrackerServer::GalileoTest < ActiveSupport::TestCase
 
   def setup
     @server = TrackerServer::Galileo.new(1234)
+  end
+
+  def validate_status_props(packet)
+    props = %w{ status sens_moving bad_angle int_power_low
+      gps_antenna int_bus_power_low ext_power_low
+      enigne_on sens_hit glonass signal alarm_mode alarm }
+    props.each{ |key| assert(packet.key?(key.to_sym), "key #{key} is absent") }
   end
 
   test "get packet size" do
@@ -76,15 +84,15 @@ class TrackerServer::GalileoTest < ActiveSupport::TestCase
     assert(packet.key? :satellites)
     assert_equal(7, packet[:satellites])
     assert(packet.key? :latitude)
-    assert_equal(53.612224, packet[:latitude])
+    assert_in_delta(53.612224, packet[:latitude], 0.0000001)
     assert(packet.key? :longitude)
-    assert_equal(86.890424, packet[:longitude])
+    assert_in_delta(86.890424, packet[:longitude], 0.0000001)
   end
 
   test "parse packet speed tag" do
     packet = @server.parse_packet("\x33\x5C\x00\x48\x08")
     assert(packet.key? :speed)
-    assert_equal(9.2, packet[:speed])
+    assert_in_delta(9.2, packet[:speed], 0.01)
     assert(packet.key? :direction)
     assert_equal(212, packet[:direction])
   end
@@ -98,16 +106,12 @@ class TrackerServer::GalileoTest < ActiveSupport::TestCase
   test "parse packet hdop tag" do
     packet = @server.parse_packet("\x35\xFF")
     assert(packet.key? :hdop)
-    assert_equal(25.5, packet[:hdop])
+    assert_in_delta(25.5, packet[:hdop], 0.01)
   end
 
   test "parse packet status0 tag" do
     packet = @server.parse_packet("\x40\x00\x00")
-    %w{status sens_moving bad_angle int_power_low
-    gps_antenna int_bus_power_low ext_power_low
-    enigne_on sens_hit glonass signal alarm_mode alarm}.each {
-      |key| assert(packet.key? key.to_sym )
-    }
+    validate_status_props(packet)
     assert_equal(0, packet[:status])
     assert_equal(false, packet[:sens_moving])
     assert_equal(false, packet[:bad_angle])
@@ -125,11 +129,7 @@ class TrackerServer::GalileoTest < ActiveSupport::TestCase
 
   test "parse packet status1 tag" do
     packet = @server.parse_packet("\x40\x01\x00")
-    %w{status sens_moving bad_angle int_power_low
-    gps_antenna int_bus_power_low ext_power_low
-    enigne_on sens_hit glonass signal alarm_mode alarm}.each {
-      |key| assert(packet.key? key.to_sym )
-    }
+    validate_status_props(packet)
     assert_equal(1, packet[:status])
     assert_equal(true, packet[:sens_moving])
     assert_equal(false, packet[:bad_angle])
@@ -147,11 +147,7 @@ class TrackerServer::GalileoTest < ActiveSupport::TestCase
 
   test "parse packet status2 tag" do
     packet = @server.parse_packet("\x40\x02\x00")
-    %w{status sens_moving bad_angle int_power_low
-    gps_antenna int_bus_power_low ext_power_low
-    enigne_on sens_hit glonass signal alarm_mode alarm}.each {
-      |key| assert(packet.key? key.to_sym )
-    }
+    validate_status_props(packet)
     assert_equal(2, packet[:status])
     assert_equal(false, packet[:sens_moving])
     assert_equal(true, packet[:bad_angle])
@@ -169,11 +165,7 @@ class TrackerServer::GalileoTest < ActiveSupport::TestCase
 
   test "parse packet status3 tag" do
     packet = @server.parse_packet("\x40\x20\x00")
-    %w{status sens_moving bad_angle int_power_low
-    gps_antenna int_bus_power_low ext_power_low
-    enigne_on sens_hit glonass signal alarm_mode alarm}.each {
-      |key| assert(packet.key? key.to_sym )
-    }
+    validate_status_props(packet)
     assert_equal(32, packet[:status])
     assert_equal(false, packet[:sens_moving])
     assert_equal(false, packet[:bad_angle])
@@ -191,11 +183,7 @@ class TrackerServer::GalileoTest < ActiveSupport::TestCase
 
   test "parse packet status4 tag" do
     packet = @server.parse_packet("\x40\x40\x00")
-    %w{status sens_moving bad_angle int_power_low
-    gps_antenna int_bus_power_low ext_power_low
-    enigne_on sens_hit glonass signal alarm_mode alarm}.each {
-      |key| assert(packet.key? key.to_sym )
-    }
+    validate_status_props(packet)
     assert_equal(64, packet[:status])
     assert_equal(false, packet[:sens_moving])
     assert_equal(false, packet[:bad_angle])
@@ -213,11 +201,7 @@ class TrackerServer::GalileoTest < ActiveSupport::TestCase
 
   test "parse packet status5 tag" do
     packet = @server.parse_packet("\x40\x80\x00")
-    %w{status sens_moving bad_angle int_power_low
-    gps_antenna int_bus_power_low ext_power_low
-    enigne_on sens_hit glonass signal alarm_mode alarm}.each {
-      |key| assert(packet.key? key.to_sym )
-    }
+    validate_status_props(packet)
     assert_equal(128, packet[:status])
     assert_equal(false, packet[:sens_moving])
     assert_equal(false, packet[:bad_angle])
@@ -235,11 +219,7 @@ class TrackerServer::GalileoTest < ActiveSupport::TestCase
 
   test "parse packet status6 tag" do
     packet = @server.parse_packet("\x40\x00\x01")
-    %w{status sens_moving bad_angle int_power_low
-    gps_antenna int_bus_power_low ext_power_low
-    enigne_on sens_hit glonass signal alarm_mode alarm}.each {
-      |key| assert(packet.key? key.to_sym )
-    }
+    validate_status_props(packet)
     assert_equal(256, packet[:status])
     assert_equal(false, packet[:sens_moving])
     assert_equal(false, packet[:bad_angle])
@@ -257,11 +237,7 @@ class TrackerServer::GalileoTest < ActiveSupport::TestCase
 
   test "parse packet status7 tag" do
     packet = @server.parse_packet("\x40\x00\x02")
-    %w{status sens_moving bad_angle int_power_low
-    gps_antenna int_bus_power_low ext_power_low
-    enigne_on sens_hit glonass signal alarm_mode alarm}.each {
-      |key| assert(packet.key? key.to_sym )
-    }
+    validate_status_props(packet)
     assert_equal(512, packet[:status])
     assert_equal(false, packet[:sens_moving])
     assert_equal(false, packet[:bad_angle])
@@ -279,11 +255,7 @@ class TrackerServer::GalileoTest < ActiveSupport::TestCase
 
   test "parse packet status8 tag" do
     packet = @server.parse_packet("\x40\x00\x04")
-    %w{status sens_moving bad_angle int_power_low
-    gps_antenna int_bus_power_low ext_power_low
-    enigne_on sens_hit glonass signal alarm_mode alarm}.each {
-      |key| assert(packet.key? key.to_sym )
-    }
+    validate_status_props(packet)
     assert_equal(1024, packet[:status])
     assert_equal(false, packet[:sens_moving])
     assert_equal(false, packet[:bad_angle])
@@ -301,11 +273,7 @@ class TrackerServer::GalileoTest < ActiveSupport::TestCase
 
   test "parse packet status9 tag" do
     packet = @server.parse_packet("\x40\x00\x08")
-    %w{status sens_moving bad_angle int_power_low
-    gps_antenna int_bus_power_low ext_power_low
-    enigne_on sens_hit glonass signal alarm_mode alarm}.each {
-      |key| assert(packet.key? key.to_sym )
-    }
+    validate_status_props(packet)
     assert_equal(2048, packet[:status])
     assert_equal(false, packet[:sens_moving])
     assert_equal(false, packet[:bad_angle])
@@ -323,11 +291,7 @@ class TrackerServer::GalileoTest < ActiveSupport::TestCase
 
   test "parse packet status10 tag" do
     packet = @server.parse_packet("\x40\x00\x30")
-    %w{status sens_moving bad_angle int_power_low
-    gps_antenna int_bus_power_low ext_power_low
-    enigne_on sens_hit glonass signal alarm_mode alarm}.each {
-      |key| assert(packet.key? key.to_sym )
-    }
+    validate_status_props(packet)
     assert_equal(12288, packet[:status])
     assert_equal(false, packet[:sens_moving])
     assert_equal(false, packet[:bad_angle])
@@ -345,11 +309,7 @@ class TrackerServer::GalileoTest < ActiveSupport::TestCase
 
   test "parse packet status11 tag" do
     packet = @server.parse_packet("\x40\x00\x40")
-    %w{status sens_moving bad_angle int_power_low
-    gps_antenna int_bus_power_low ext_power_low
-    enigne_on sens_hit glonass signal alarm_mode alarm}.each {
-      |key| assert(packet.key? key.to_sym )
-    }
+    validate_status_props(packet)
     assert_equal(16384, packet[:status])
     assert_equal(false, packet[:sens_moving])
     assert_equal(false, packet[:bad_angle])
@@ -367,11 +327,7 @@ class TrackerServer::GalileoTest < ActiveSupport::TestCase
 
   test "parse packet status12 tag" do
     packet = @server.parse_packet("\x40\x00\x80")
-    %w{status sens_moving bad_angle int_power_low
-    gps_antenna int_bus_power_low ext_power_low
-    enigne_on sens_hit glonass signal alarm_mode alarm}.each {
-      |key| assert(packet.key? key.to_sym )
-    }
+    validate_status_props(packet)
     assert_equal(32768, packet[:status])
     assert_equal(false, packet[:sens_moving])
     assert_equal(false, packet[:bad_angle])
@@ -427,28 +383,13 @@ class TrackerServer::GalileoTest < ActiveSupport::TestCase
     assert_equal(23130, packet[:input_statuses])
   end
 
-  test "parse packet power_input_0 tag" do
-    packet = @server.parse_packet("\x50\x5A\x5A")
-    assert(packet.key? :power_input_0)
-    assert_equal(23130, packet[:power_input_0])
-  end
-
-  test "parse packet power_input_1 tag" do
-    packet = @server.parse_packet("\x51\x5A\x5A")
-    assert(packet.key? :power_input_1)
-    assert_equal(23130, packet[:power_input_1])
-  end
-
-  test "parse packet power_input_2 tag" do
-    packet = @server.parse_packet("\x52\x5A\x5A")
-    assert(packet.key? :power_input_2)
-    assert_equal(23130, packet[:power_input_2])
-  end
-
-  test "parse packet power_input_3 tag" do
-    packet = @server.parse_packet("\x53\x5A\x5A")
-    assert(packet.key? :power_input_3)
-    assert_equal(23130, packet[:power_input_3])
+  test "parse packet power_input tags" do
+    0.upto(3).each do |tag_index|
+      packet = @server.parse_packet((0x50 + tag_index).chr + "\x5A\x5A")
+      tag_key = "power_input_#{tag_index}".to_sym
+      assert(packet.key?(tag_key), tag_key)
+      assert_equal(23130, packet[tag_key], tag_key)
+    end
   end
 
   test "parse packet rs232_0 tag" do
@@ -469,46 +410,13 @@ class TrackerServer::GalileoTest < ActiveSupport::TestCase
     assert_equal(16, packet[:thermometer_0])
   end
 
-  test "parse packet thermometer_1 tag" do
-    packet = @server.parse_packet("\x71\x01\xD8")
-    assert(packet.key? :thermometer_1)
-    assert_equal(-40, packet[:thermometer_1])
-  end
-
-  test "parse packet thermometer_2 tag" do
-    packet = @server.parse_packet("\x72\x02\xD8")
-    assert(packet.key? :thermometer_2)
-    assert_equal(-40, packet[:thermometer_2])
-  end
-
-  test "parse packet thermometer_3 tag" do
-    packet = @server.parse_packet("\x73\x03\xD8")
-    assert(packet.key? :thermometer_3)
-    assert_equal(-40, packet[:thermometer_3])
-  end
-
-  test "parse packet thermometer_4 tag" do
-    packet = @server.parse_packet("\x74\x04\xD8")
-    assert(packet.key? :thermometer_4)
-    assert_equal(-40, packet[:thermometer_4])
-  end
-
-  test "parse packet thermometer_5 tag" do
-    packet = @server.parse_packet("\x75\x05\xD8")
-    assert(packet.key? :thermometer_5)
-    assert_equal(-40, packet[:thermometer_5])
-  end
-
-  test "parse packet thermometer_6 tag" do
-    packet = @server.parse_packet("\x76\x06\xD8")
-    assert(packet.key? :thermometer_6)
-    assert_equal(-40, packet[:thermometer_6])
-  end
-
-  test "parse packet thermometer_7 tag" do
-    packet = @server.parse_packet("\x77\x07\xD8")
-    assert(packet.key? :thermometer_7)
-    assert_equal(-40, packet[:thermometer_7])
+  test "parse packet thermometers tags" do
+    1.upto(7).each do |tag_index|
+      packet = @server.parse_packet((0x70 + tag_index).chr + "\x01\xD8")
+      tag_key = "thermometer_#{tag_index}".to_sym
+      assert(packet.key?(tag_key), tag_key)
+      assert_equal(-40, packet[tag_key], tag_key)
+    end
   end
 
   test "parse packet ibutton tag" do
@@ -545,172 +453,31 @@ class TrackerServer::GalileoTest < ActiveSupport::TestCase
     assert_equal(67305985, packet[:can_b1])
   end
 
-  test "parse packet can8bitr0 tag" do
-    packet = @server.parse_packet("\xC4\xA5")
-    assert(packet.key? :can8bitr0)
-    assert_equal(165, packet[:can8bitr0])
+  test "parse packet can8bitr tags" do
+    0.upto(17).each do |tag_index|
+      packet = @server.parse_packet((0xC4 + tag_index).chr + "\xA5")
+      tag_key = "can8bitr#{tag_index}".to_sym
+      assert(packet.key?(tag_key), tag_key)
+      assert_equal(165, packet[tag_key], tag_key)
+    end
   end
 
-  test "parse packet can8bitr1 tag" do
-    packet = @server.parse_packet("\xC5\xA5")
-    assert(packet.key? :can8bitr1)
-    assert_equal(165, packet[:can8bitr1])
+  test "parse packet can16bitr tags" do
+    0.upto(4).each do |tag_index|
+      packet = @server.parse_packet((0xD6 + tag_index).chr + "\x5A\x5A")
+      tag_key = "can16bitr#{tag_index}".to_sym
+      assert(packet.key?(tag_key), tag_key)
+      assert_equal(23130, packet[tag_key], tag_key)
+    end 
   end
 
-  test "parse packet can8bitr2 tag" do
-    packet = @server.parse_packet("\xC6\xA5")
-    assert(packet.key? :can8bitr2)
-    assert_equal(165, packet[:can8bitr2])
-  end
-
-  test "parse packet can8bitr3 tag" do
-    packet = @server.parse_packet("\xC7\xA5")
-    assert(packet.key? :can8bitr3)
-    assert_equal(165, packet[:can8bitr3])
-  end
-
-  test "parse packet can8bitr4 tag" do
-    packet = @server.parse_packet("\xC8\xA5")
-    assert(packet.key? :can8bitr4)
-    assert_equal(165, packet[:can8bitr4])
-  end
-
-  test "parse packet can8bitr5 tag" do
-    packet = @server.parse_packet("\xC9\xA5")
-    assert(packet.key? :can8bitr5)
-    assert_equal(165, packet[:can8bitr5])
-  end
-
-  test "parse packet can8bitr6 tag" do
-    packet = @server.parse_packet("\xCA\xA5")
-    assert(packet.key? :can8bitr6)
-    assert_equal(165, packet[:can8bitr6])
-  end
-
-  test "parse packet can8bitr7 tag" do
-    packet = @server.parse_packet("\xCB\xA5")
-    assert(packet.key? :can8bitr7)
-    assert_equal(165, packet[:can8bitr7])
-  end
-
-  test "parse packet can8bitr8 tag" do
-    packet = @server.parse_packet("\xCC\xA5")
-    assert(packet.key? :can8bitr8)
-    assert_equal(165, packet[:can8bitr8])
-  end
-
-  test "parse packet can8bitr9 tag" do
-    packet = @server.parse_packet("\xCD\xA5")
-    assert(packet.key? :can8bitr9)
-    assert_equal(165, packet[:can8bitr9])
-  end
-
-  test "parse packet can8bitr10 tag" do
-    packet = @server.parse_packet("\xCE\xA5")
-    assert(packet.key? :can8bitr10)
-    assert_equal(165, packet[:can8bitr10])
-  end
-
-  test "parse packet can8bitr11 tag" do
-    packet = @server.parse_packet("\xCF\xA5")
-    assert(packet.key? :can8bitr11)
-    assert_equal(165, packet[:can8bitr11])
-  end
-
-  test "parse packet can8bitr12 tag" do
-    packet = @server.parse_packet("\xD0\xA5")
-    assert(packet.key? :can8bitr12)
-    assert_equal(165, packet[:can8bitr12])
-  end
-
-  test "parse packet can8bitr13 tag" do
-    packet = @server.parse_packet("\xD1\xA5")
-    assert(packet.key? :can8bitr13)
-    assert_equal(165, packet[:can8bitr13])
-  end
-
-  test "parse packet can8bitr14 tag" do
-    packet = @server.parse_packet("\xD2\xA5")
-    assert(packet.key? :can8bitr14)
-    assert_equal(165, packet[:can8bitr14])
-  end
-
-  test "parse packet can8bitr15 tag" do
-    packet = @server.parse_packet("\xD3\xA5")
-    assert(packet.key? :can8bitr15)
-    assert_equal(165, packet[:can8bitr15])
-  end
-
-  test "parse packet can8bitr16 tag" do
-    packet = @server.parse_packet("\xD4\xA5")
-    assert(packet.key? :can8bitr16)
-    assert_equal(165, packet[:can8bitr16])
-  end
-
-  test "parse packet can8bitr17 tag" do
-    packet = @server.parse_packet("\xD5\xA5")
-    assert(packet.key? :can8bitr17)
-    assert_equal(165, packet[:can8bitr17])
-  end
-
-  test "parse packet can16bitr0 tag" do
-    packet = @server.parse_packet("\xD6\x5A\x5A")
-    assert(packet.key? :can16bitr0)
-    assert_equal(23130, packet[:can16bitr0])
-  end
-
-  test "parse packet can16bitr1 tag" do
-    packet = @server.parse_packet("\xD7\x5A\x5A")
-    assert(packet.key? :can16bitr1)
-    assert_equal(23130, packet[:can16bitr1])
-  end
-
-  test "parse packet can16bitr2 tag" do
-    packet = @server.parse_packet("\xD8\x5A\x5A")
-    assert(packet.key? :can16bitr2)
-    assert_equal(23130, packet[:can16bitr2])
-  end
-
-  test "parse packet can16bitr3 tag" do
-    packet = @server.parse_packet("\xD9\x5A\x5A")
-    assert(packet.key? :can16bitr3)
-    assert_equal(23130, packet[:can16bitr3])
-  end
-
-  test "parse packet can16bitr4 tag" do
-    packet = @server.parse_packet("\xDA\x5A\x5A")
-    assert(packet.key? :can16bitr4)
-    assert_equal(23130, packet[:can16bitr4])
-  end
-
-  test "parse packet can32bitr0 tag" do
-    packet = @server.parse_packet("\xDB\x01\x02\x03\x04")
-    assert(packet.key? :can32bitr0)
-    assert_equal(67305985, packet[:can32bitr0])
-  end
-
-  test "parse packet can32bitr1 tag" do
-    packet = @server.parse_packet("\xDC\x01\x02\x03\x04")
-    assert(packet.key? :can32bitr1)
-    assert_equal(67305985, packet[:can32bitr1])
-  end
-
-  test "parse packet can32bitr2 tag" do
-    packet = @server.parse_packet("\xDD\x01\x02\x03\x04")
-    assert(packet.key? :can32bitr2)
-    assert_equal(67305985, packet[:can32bitr2])
-  end
-
-  test "parse packet can32bitr3 tag" do
-    packet = @server.parse_packet("\xDE\x01\x02\x03\x04")
-    assert(packet.key? :can32bitr3)
-    assert_equal(67305985, packet[:can32bitr3])
-  end
-
-  test "parse packet can32bitr4 tag" do
-    packet = @server.parse_packet("\xDF\x01\x02\x03\x04")
-    assert(packet.key? :can32bitr4)
-    assert_equal(67305985, packet[:can32bitr4])
+  test "parse packet can32bitr tags" do
+    0.upto(4).each do |tag_index|
+      packet = @server.parse_packet((0xDB + tag_index).chr + "\x01\x02\x03\x04")
+      tag_key = "can32bitr#{tag_index}".to_sym
+      assert(packet.key?(tag_key), tag_key)
+      assert_equal(67305985, packet[tag_key], tag_key)
+    end
   end
 
 end
