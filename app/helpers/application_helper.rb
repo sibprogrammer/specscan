7b36196f8322b895c57aaac1d66a5fb2de17f648
Alexei Yuzhakov <sibprogrammer@gmail.com>
2012-02-14 22:43:38 +0700

    Implement context help in sidebar.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,17 @@
 module ApplicationHelper
+
+  def context_help?(help)
+    help || ('' != t(get_locale_section_for_context + ".help", :default => ''))
+  end
+
+  def context_help(help)
+    help || t(get_locale_section_for_context + ".help")
+  end
+
+  private
+
+    def get_locale_section_for_context
+      "#{params[:controller].sub('/', '.')}.#{params[:action]}"
+    end
+
 end
