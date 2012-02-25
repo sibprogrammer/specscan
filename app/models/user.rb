f06b42c81c39d1b3f432dc2a40c2408e368fe660
Alexei Yuzhakov <sibprogrammer@gmail.com>
2012-02-25 19:43:32 +0700

    Bunch of tests for user model.

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,10 +4,13 @@ class User < ActiveRecord::Base
   ROLE_CLIENT = 2
   ROLES = [['admin', ROLE_ADMIN], ['client', ROLE_CLIENT]]
 
-  validates :login, :presence => true, :uniqueness => true, :length => { :minimum => 3 }
+  validates :login, :presence => true, :uniqueness => true, :length => { :minimum => 3, :maximum => 15 },
+    :format => { :with => /^[a-z\d]+$/ }
+  validates :password, :presence => true, :confirmation => true, :on => :create,
+    :length => { :minimum => 6, :maximum => 25 }, :format => { :with => /^[a-zA-Z\d]+$/ }
+  validates :role, :presence => true
   devise :database_authenticatable, :rememberable, :trackable, :validatable
   attr_accessible :login, :name, :email, :password, :password_confirmation, :remember_me, :role
-  validates :password, :presence => true, :confirmation => true, :on => :create
 
   has_many :vehicles
 
@@ -24,6 +27,10 @@ class User < ActiveRecord::Base
       when ROLE_ADMIN then 'admin'
       when ROLE_CLIENT then 'client'
     end
+  end
+
+  def login=(value)
+    write_attribute :login, (value ? value.downcase : nil)
   end
 
   protected
--- test/unit/user_test.rb
+++ test/unit/user_test.rb
@@ -1,7 +1,101 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  test "should not save user without required attributes" do
+    user = User.new
+    assert !user.save
+  end
+
+  test "valid user with only required attributes filled" do
+    user = User.new(:login => 'test', :password => 'test123', :role => 2)
+    assert user.valid?
+  end
+
+  test "user should have role defined" do
+    user = User.new(:login => 'test', :password => 'test123', :role => nil)
+    assert user.invalid?
+  end
+
+  test "user with shortest login" do
+    user = User.new(:login => 'abc', :password => 'test123')
+    assert user.valid?
+  end
+
+  test "user with too short login" do
+    user = User.new(:login => 'ab', :password => 'test123')
+    assert user.invalid?
+  end
+
+  test "user with longest login" do
+    user = User.new(:login => '123456789012345', :password => 'test123')
+    assert user.valid?
+  end
+
+  test "user with too long login" do
+    user = User.new(:login => '1234567890123456', :password => 'test123')
+    assert user.invalid?
+  end
+
+  test "user logins are always in lower case" do
+    user = User.new(:login => 'CAPS', :password => 'test123', :email => 'email@example.dom')
+    assert_equal 'caps', user.login
+  end
+
+  test "user login should not contain special characters" do
+    user = User.new(:login => 'test!', :password => '123456')
+    assert user.invalid?
+  end
+
+  test "user password should not contain special characters" do
+    user = User.new(:login => 'test', :password => 'test123!')
+    assert user.invalid?
+  end
+
+  test "user with shortest password" do
+    user = User.new(:login => 'test', :password => '123456')
+    assert user.valid?
+  end
+
+  test "user with too short password" do
+    user = User.new(:login => 'test', :password => '12345')
+    assert user.invalid?
+  end
+
+  test "user with longest password" do
+    user = User.new(:login => 'test', :password => '1234567890123456789012345')
+    assert user.valid?
+  end
+
+  test "user with too long password" do
+    user = User.new(:login => 'test', :password => '12345678901234567890123456')
+    assert user.invalid?
+  end
+
+  test "user with email" do
+    user = User.new(:login => 'test', :password => 'test123', :email => 'email@example.dom')
+    assert user.valid?
+  end
+
+  test "user should have valid email" do
+    user = User.new(:login => 'test', :password => 'test123', :email => 'invalid')
+    assert user.invalid?
+  end
+
+  test "user with role admin" do
+    assert users(:admin).admin?
+  end
+
+  test "user with role client" do
+    assert users(:client).client?
+  end
+
+  test "admin user role name" do
+    assert_equal 'admin', users(:admin).role_name
+  end
+
+  test "client user role name" do
+    assert_equal 'client', users(:client).role_name
+  end
+
 end
