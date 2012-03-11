e3c657ce3a8526e721daee02ece0a1d8b2b0df57
Alexei Yuzhakov <sibprogrammer@gmail.com>
2012-03-11 23:32:56 +0700

    Use Havershine algorithm for distance calculations, 'cause it's a little bit faster.

--- app/models/way_point.rb
+++ app/models/way_point.rb
@@ -14,33 +14,13 @@ class WayPoint
   end
 
   def distance(to_point)
-    coors_to_distance_vector(latitude, longitude, to_point.latitude, to_point.longitude)
+    coors_to_distance_haversine(latitude, longitude, to_point.latitude, to_point.longitude)
   end
 
-private
+  private
 
     def to_rad(ang)
       ang * Math::PI / 180
-    end
-
-    def vector_module(c_x, c_y, c_z)
-      Math.sqrt(c_x**2 + c_y**2 + c_z**2)
-    end
-
-    def geosph_to_xyz(rad, lat, long)
-      lat, long = to_rad(lat), to_rad(long)
-      x = rad * Math.cos(lat) * Math.cos(long)
-      y = rad * Math.cos(lat) * Math.sin(long)
-      z = rad * Math.sin(lat)
-      [x, y, z]
-    end
-
-    def coors_to_distance_vector(lat1, long1, lat2, long2)
-      # hand-made algorithm by Igor Vayzert
-      r = 6_371_000 # radius of the Earth
-      x1, y1, z1 = geosph_to_xyz(r, lat1, long1)
-      x2, y2, z2 = geosph_to_xyz(r, lat2, long2)
-      vector_module(x2 - x1, y2 - y1, z2 - z1)
     end
 
     def coors_to_distance_haversine(lat1, long1, lat2, long2)
