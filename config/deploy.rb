af2968a81c51c181ee461c6c15861a7060b52d5a
Alexei Yuzhakov <sibprogrammer@gmail.com>
2012-04-24 00:10:37 +0700

    Add deploy script for automated updates of production server.

---
+++ config/deploy.rb
@@ -0,0 +1,48 @@
+set :application, 'specscan'
+set :repository, 'git@github.com:sibprogrammer/specscan.git'
+set :scm, :git
+set :deploy_via, :remote_cache
+set :deploy_to, '/var/www/panel'
+set :user, 'deployer'
+
+set :default_environment, {
+  'PATH' => "/usr/local/rvm/gems/ree-1.8.7-2012.02/bin:/usr/local/rvm/gems/ree-1.8.7-2012.02@global/bin:/usr/local/rvm/rubies/ree-1.8.7-2012.02/bin:/usr/local/rvm/bin:$PATH",
+  'RUBY_VERSION' => 'ruby 1.8.7',
+  'GEM_HOME' => '/usr/local/rvm/gems/ree-1.8.7-2012.02',
+  'GEM_PATH' => '/usr/local/rvm/gems/ree-1.8.7-2012.02:/usr/local/rvm/gems/ree-1.8.7-2012.02@global',
+  'BUNDLE_PATH' => '/usr/local/rvm/gems/ree-1.8.7-2012.02@global'
+}
+
+role :web, 'specscan.ru'
+role :app, 'specscan.ru'
+role :db, 'specscan.ru', :primary => true
+
+after 'deploy:update_code', 'deploy:symlink_configs'
+after 'deploy:update_code', 'deploy:precompile'
+after 'deploy:update_code', 'deploy:fix_permissions'
+after "deploy:restart", "deploy:cleanup"
+
+namespace :deploy do
+  task :start do ; end
+  task :stop do ; end
+  task :restart, :roles => :app, :except => { :no_release => true } do
+    run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
+  end
+
+  desc "Symlink configs"
+  task :symlink_configs, :roles => :app do
+    %w{ database.yml config.yml }.each do |file|
+      run "ln -nfs #{deploy_to}/shared/config/#{file} #{release_path}/config/#{file}"
+    end
+  end
+
+  task :precompile, :role => :app do
+    run "cd #{release_path}/ && rake assets:precompile"
+  end
+
+  task :fix_permissions, :role => :app do
+    %w{ config.ru config/environment.rb db log tmp }.each do |file|
+      run "sudo chown -R www-data:www-data #{release_path}/#{file}"
+    end
+  end
+end
