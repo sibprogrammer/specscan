f2f1459d11b0f932e834cb3d4965823b9fac7b35
Alexei Yuzhakov <sibprogrammer@gmail.com>
2012-05-01 23:48:02 +0700

    Show SIM-cards total number on dashboard stats for admins.

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -3,6 +3,7 @@ class Admin::DashboardController < Admin::Base
   def index
     @users_total = User.count(:all) if can? :manage, User
     @vehicles_total = can?(:manage, Vehicle) ? Vehicle.count(:all) : current_user.vehicles.count
+    @sim_cards_total = SimCard.count(:all) if can? :manage, SimCard
   end
 
 end
