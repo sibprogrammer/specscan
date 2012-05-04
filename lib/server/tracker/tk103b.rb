45b5deffca9a23c40775a21dc8d90758d8f61171
Alexei Yuzhakov <sibprogrammer@gmail.com>
2012-05-04 23:03:07 +0700

    Improvements on TK103B protocol parsing: force tracker to send data, proper coords and speed handling.

--- lib/server/tracker/tk103b.rb
+++ lib/server/tracker/tk103b.rb
@@ -2,24 +2,32 @@ require 'server/tracker/abstract'
 
 class Server::Tracker::Tk103b < Server::Tracker::Abstract
 
-  def process_data(client)
-    header = read_data(client)
-    raise "Unrecognized header" unless header.starts_with?('##')
-    send_data(client, 'LOAD')
+  SPEED_KM_PER_KNOT = 1.852
+  REPLY_INTERVAL = '5s'
 
+  def process_data(client)
     loop do
       data = read_data(client)
-      if data.match /^\d+/
+
+      if data.starts_with?('##')
+        # process header
+        send_data(client, 'LOAD')
+        imei = data.split(',')[1].split(':').last
+        logger.debug "Tracker IMEI: #{imei}"
+        send_data(client, "**,imei:#{imei},C,#{REPLY_INTERVAL}")
+      elsif data.match /^\d+/
         # answer on heartbeat
         send_data(client, 'ON')
-        next
+      elsif data.starts_with?('imei:')
+        # process regular packet
+        packet = parse_packet(data)
+        logger.debug packet.inspect
+
+        point = WayPoint.new(packet)
+        point.save
+      else
+        raise "Unrecognized data packet #{data}"
       end
-
-      packet = parse_packet(data)
-      logger.debug packet.inspect
-
-      point = WayPoint.new(packet)
-      point.save
     end
   end
 
@@ -53,16 +61,20 @@ class Server::Tracker::Tk103b < Server::Tracker::Abstract
       packet[:coors_valid] = gps_ok and data_ok
 
       if packet[:coors_valid]
-        packet[:latitude] = fields[7].to_f / 100
+        packet[:latitude] = coors_to_degrees(fields[7].to_f)
         packet[:latitude] = -packet[:latitude] if 'S' == fields[8]
-        packet[:longitude] = fields[9].to_f / 100
+        packet[:longitude] = coors_to_degrees(fields[9].to_f)
         packet[:longitude] = -packet[:longitude] if 'W' == fields[10]
-        packet[:speed] = fields[11].to_f
+        packet[:speed] = fields[11].to_f * SPEED_KM_PER_KNOT
         packet[:engine_on] = packet[:speed] > 0.001
         packet[:sens_moving] = packet[:speed] > 0.001
       end
 
       packet
+    end
+
+    def coors_to_degrees(coor)
+      (coor / 100).truncate + ((coor / 100).remainder(1) * 10 / 6)
     end
 
 end
