02959b16cdae3fe1e3577ba5fd909f1b0d76b0da
Alexei Yuzhakov <sibprogrammer@gmail.com>
2012-07-09 23:23:40 +0700

    Unit tests for vehicle type model.

--- app/models/vehicle_type.rb
+++ app/models/vehicle_type.rb
@@ -1,6 +1,6 @@
 class VehicleType < ActiveRecord::Base
 
-  validates :code, :presence => true, :uniqueness => true
+  validates :code, :presence => true, :uniqueness => true, :format => { :with => /\A[a-z]+\z/ }
   validates :title, :presence => true, :uniqueness => true
 
 end
--- test/unit/vehicle_type_test.rb
+++ test/unit/vehicle_type_test.rb
@@ -1,7 +1,53 @@
 require 'test_helper'
 
 class VehicleTypeTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  test "should not save vehicle type without required attributes" do
+    vehicle_type = VehicleType.new
+    assert !vehicle_type.save
+  end
+
+  test "valid vehicle type" do
+    vehicle_type = VehicleType.new(:title => 'Truck', :code => 'truck')
+    assert vehicle_type.valid?
+  end
+
+  test "vehicle type code should be unique" do
+    vehicle_type = VehicleType.new(:title => 'Truck', :code => 'truck')
+    assert vehicle_type.save
+
+    another_vehicle_type = VehicleType.new(:title => 'Car', :code => 'truck')
+    assert another_vehicle_type.invalid?
+  end
+
+  test "vehicle type title should be unique" do
+    vehicle_type = VehicleType.new(:title => 'Truck', :code => 'truck')
+    assert vehicle_type.save
+
+    another_vehicle_type = VehicleType.new(:title => 'Truck', :code => 'car')
+    assert another_vehicle_type.invalid?
+  end
+
+  test "valid vehicle type code" do
+    vehicle_type = VehicleType.new(:title => 'Truck')
+
+    vehicle_type.code = 'truck'
+    assert vehicle_type.valid?
+
+    vehicle_type.code = 'truck123'
+    assert vehicle_type.invalid?
+
+    vehicle_type.code = 'abc 123'
+    assert vehicle_type.invalid?
+
+    vehicle_type.code = 'abc-123'
+    assert vehicle_type.invalid?
+
+    vehicle_type.code = 'abc#$%'
+    assert vehicle_type.invalid?
+
+    vehicle_type.code = 'ABC'
+    assert vehicle_type.invalid?
+  end
+
 end
