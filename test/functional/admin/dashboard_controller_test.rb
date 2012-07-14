68b52bf5e2ca41dd5fe35f800e15c3c6e12dda4c
Alexei Yuzhakov <sibprogrammer@gmail.com>
2012-07-14 09:21:33 +0700

    Add example of functional test.

--- test/functional/admin/dashboard_controller_test.rb
+++ test/functional/admin/dashboard_controller_test.rb
@@ -1,9 +1,17 @@
 require 'test_helper'
 
 class Admin::DashboardControllerTest < ActionController::TestCase
-  #test "should get index" do
-  #  get :index
-  #  assert_response :success
-  #end
+
+  include Devise::TestHelpers
+
+  def setup
+    @request.env["devise.mapping"] = Devise.mappings[:admin]
+    sign_in users(:admin)
+  end
+
+  test "should get index" do
+    get :index
+    assert_response :success
+  end
 
 end
